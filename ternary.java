9116b5d4d6e47bb85db10003942ffa8e4f27d5c8
9488711057 <srisankariumapathi@gmail.com>
2026-01-09 09:53:42 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    int b=30;
+	    int max=(a > b) ?a : b;
+	    int min=(a < b) ?a : b;
+		{
+		    System.out.println("Maximum value is: " + max);
+		    System.out.println("Minimum value is: " + min);
+	}
+}
+}

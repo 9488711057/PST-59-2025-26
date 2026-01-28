0a9c97c5fed3152dc1547f5fd7c57a59ca060ad2
Sri Sankari U <srisankariumapathi@gmail.com>
2026-01-28 10:17:18 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,18 @@
+package pst;
+import java.util.Scanner;
+
+public class sum {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);  
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        int sum = a + b + c + d + e;
+        System.out.println("Sum: " + sum);
+	}
+
+}

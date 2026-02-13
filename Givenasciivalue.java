098384a2d00b41dfe42e8a9ffe96aa7463197d4c
Sri Sankari U <srisankariumapathi@gmail.com>
2026-02-13 09:19:51 +0530

    Add files via upload

---
+++ Givenasciivalue.java
@@ -0,0 +1,18 @@
+package pst;
+import java.util.Scanner;
+public class Givenasciivalue {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the ASCII value: ");
+        int ascii = sc.nextInt();
+
+        printChar(ascii);
+        sc.close();
+    }
+
+    public static void printChar(int ascii) {
+        char ch = (char) ascii;
+        System.out.println("Character for ASCII value " + ascii + " is: " + ch);
+    }
+}

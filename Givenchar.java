943b10464c7ebc09f703c09f41f57ff406d57b77
Sri Sankari U <srisankariumapathi@gmail.com>
2026-02-14 11:48:11 +0530

    Add files via upload

---
+++ Givenchar.java
@@ -0,0 +1,20 @@
+package pst;
+
+import java.util.Scanner;
+
+public class Givenchar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        printAscii(ch);
+    }
+
+    public static void printAscii(char ch) {
+        int ascii = ch;  
+        System.out.println("ASCII value of " + ch + " is: " + ascii);
+    }
+}
